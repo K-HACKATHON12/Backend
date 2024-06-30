7cce32edef938e963e1189797f1744307a741c63
tomato <tomatomato2121@gmail.com>
2024-07-01 01:09:30 +0900

    hotfix: rename function and var name from ctrl.api

--- ctrl/api.py
+++ ctrl/api.py
@@ -8,15 +8,15 @@ from fastapi import Depends, APIRouter, HTTPException
 ctrl_router = APIRouter()
 
 @ctrl_router.get("/", response_model=List[PopulationWorkplacePydantic])
-def read_facility_info(db: Session = Depends(get_db)):
-    facility_info = db.query(PopulationWorkplace).limit(100).all()
-    if not facility_info:
-        raise HTTPException(status_code=404, detail="Facility not found")
-    return facility_info
+def population_workplace(db: Session = Depends(get_db)):
+    population = db.query(PopulationWorkplace).limit(100).all()
+    if not population:
+        raise HTTPException(status_code=404, detail="Population not found")
+    return population
 
 @ctrl_router.get("/len", response_model=int)
-def len_info(db: Session = Depends(get_db)):
-    facility_info = db.query(PopulationWorkplace).limit(100).all()
-    if not facility_info:
-        raise HTTPException(status_code=404, detail="Facility not found")
-    return len(facility_info)
+def population_workplace_len(db: Session = Depends(get_db)):
+    population = db.query(PopulationWorkplace).limit(100).all()
+    if not population:
+        raise HTTPException(status_code=404, detail="Population not found")
+    return len(population)
