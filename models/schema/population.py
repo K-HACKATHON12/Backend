c37de34675b19730ad058264dda261c3419c4031
tomato <tomatomato2121@gmail.com>
2024-07-04 02:43:09 +0900

    Beta complate

--- models/schema/population.py
+++ models/schema/population.py
@@ -1,89 +1,90 @@
 from pydantic import BaseModel
 
 class PopulationStreetPydantic(BaseModel):
-    id: int
-    trdar_cd: int
-    trdar_cd_nm: str
-    trdar_se_cd: str
-    trdar_se_cd_nm: str
-    stdr_yyqu_cd: int
+    ID: int
+    TRDAR_CD: int
+    TRDAR_CD_NM: str
+    TRDAR_SE_CD: str
+    TRDAR_SE_CD_NM: str
+    STDR_YYQU_CD: int
 
     # 남성 유동인구 수
-    ml_flpop_co: int
+    ML_FLPOP_CO: int
 
     # 여성 유동인구 수
-    fml_flpop_co: int
+    FML_FLPOP_CO: int
 
     # 요일별 유동인구 수
-    sun_flpop_co: int
-    mon_flpop_co: int
-    tues_flpop_co: int
-    wed_flpop_co: int
-    thur_flpop_co: int
-    fri_flpop_co: int
-    sat_flpop_co: int
+    SUN_FLPOP_CO: int
+    MON_FLPOP_CO: int
+    TUES_FLPOP_CO: int
+    WED_FLPOP_CO: int
+    THUR_FLPOP_CO: int
+    FRI_FLPOP_CO: int
+    SAT_FLPOP_CO: int
 
     # 연령별 유동인구 수
-    agrde_10_flpop_co: int
-    agrde_20_flpop_co: int
-    agrde_30_flpop_co: int
-    agrde_40_flpop_co: int
-    agrde_50_flpop_co: int
-    agrde_60_above_flpop_co: int
+    AGRDE_10_FLPOP_CO: int
+    AGRDE_20_FLPOP_CO: int
+    AGRDE_30_FLPOP_CO: int
+    AGRDE_40_FLPOP_CO: int
+    AGRDE_50_FLPOP_CO: int
+    AGRDE_60_ABOVE_FLPOP_CO: int
 
     # 시간대
-    tot_flpop_co: int
-    tmzon_00_06_flpop_co: int
-    tmzon_06_11_flpop_co: int
-    tmzon_11_14_flpop_co: int
-    tmzon_14_17_flpop_co: int
-    tmzon_17_21_flpop_co: int
-    tmzon_21_24_flpop_co: int
+    TOT_FLPOP_CO: int
+    TMZON_00_06_FLPOP_CO: int
+    TMZON_06_11_FLPOP_CO: int
+    TMZON_11_14_FLPOP_CO: int
+    TMZON_14_17_FLPOP_CO: int
+    TMZON_17_21_FLPOP_CO: int
+    TMZON_21_24_FLPOP_CO: int
 
     class Config:
         orm_mode = True
 
 class PopulationResidentPydantic(BaseModel):
-    id: int
-    trdar_cd: int
-    trdar_cd_nm: str
-    trdar_se_cd: str
-    trdar_se_cd_nm: str
-    stdr_yyqu_cd: int
-    tot_repop_co: int
+    ID: int
+    TRDAR_CD: int
+    TRDAR_CD_NM: str
+    TRDAR_SE_CD: str
+    TRDAR_SE_CD_NM: str
+    STDR_YYQU_CD: int
+    TOT_REPOP_CO: int
 
     # 남성 연령대별 상주인구 수
-    mag_10_repop_co: int
-    mag_20_repop_co: int
-    mag_30_repop_co: int
-    mag_40_repop_co: int
-    mag_50_repop_co: int
-    mag_60_above_repop_co: int
+    MAG_10_REPOP_CO: int
+    MAG_20_REPOP_CO: int
+    MAG_30_REPOP_CO: int
+    MAG_40_REPOP_CO: int
+    MAG_50_REPOP_CO: int
+    MAG_60_ABOVE_REPOP_CO: int
 
     # 여성 연령대별 상주인구 수
-    fag_10_repop_co: int
-    fag_20_repop_co: int
-    fag_30_repop_co: int
-    fag_40_repop_co: int
-    fag_50_repop_co: int
-    fag_60_above_repop_co: int
+    FAG_10_REPOP_CO: int
+    FAG_20_REPOP_CO: int
+    FAG_30_REPOP_CO: int
+    FAG_40_REPOP_CO: int
+    FAG_50_REPOP_CO: int
+    FAG_60_ABOVE_REPOP_CO: int
 
     # 연령대별 상주인구 수
-    agrde_10_repop_co: int
-    agrde_20_repop_co: int
-    agrde_30_repop_co: int
-    agrde_40_repop_co: int
-    agrde_50_repop_co: int
-    agrde_60_above_repop_co: int
+    AGRDE_10_REPOP_CO: int
+    AGRDE_20_REPOP_CO: int
+    AGRDE_30_REPOP_CO: int
+    AGRDE_40_REPOP_CO: int
+    AGRDE_50_REPOP_CO: int
+    AGRDE_60_ABOVE_REPOP_CO: int
 
     # 성별 상주인구 수
-    ml_repop_co: int
-    fml_repop_co: int
+    ML_REPOP_CO: int
+    FML_REPOP_CO: int
 
     # 가구수
-    apt_hshld_co: int
-    non_apt_hshld_co: int
-    tot_hshld_co: int
+    APT_HSHLD_CO: int
+    NON_APT_HSHLD_CO: int
+    TOT_HSHLD_CO: int
+
 
     class Config:
         orm_mode = True
